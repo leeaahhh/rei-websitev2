4ce6c8d114bebe4510d140f49c2889a937f403d0
leah <125518381+leeaahhh@users.noreply.github.com>
2024-11-04 23:37:22 +0100

    aaa

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://rei.nerv.run",
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: vercel({
+    functionPerRoute: false
+  }),
+});
